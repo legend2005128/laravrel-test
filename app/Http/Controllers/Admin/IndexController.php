9af12d1af25269364f3b1ffeef90eed8b17b675d
legend2005128 <hao2005128@126.com>
2016-12-15 15:45:01 +0800

    ''

---
+++ app/Http/Controllers/Admin/IndexController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class IndexController extends Controller
+{
+    public function index()
+    {
+            return 'ok';
+    }
+    public function login()
+    {
+
+       return view('admin.login');
+    }
+}
---
+++ resources/views/ini/admin.blade.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <title>App Name - @yield('title')</title>
+    <link href="//cdn.bootcss.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+    @yield('content')
+</div>
+<script src="//cdn.bootcss.com/bootstrap/4.0.0-alpha.5/js/bootstrap.min.js"></script>
+</body>
+</html>
